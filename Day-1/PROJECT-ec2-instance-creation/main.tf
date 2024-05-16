11b0a4e7fdfec45697d055f49fd5f2ac3fd90ecd
pradeepks034 <141991852+pradeepks034@users.noreply.github.com>
2024-05-16 03:25:30 +0000

    updated AMI image id & tag addition

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0e001c9271cf7f3b9"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    tags = {
+        Name = "Terra1"
+    }
 }
